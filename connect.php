78cfc30c6e5b8fbe00864c16509d3a4c529b1b05
Tirta Mahardika <37982937+tirtamahardika@users.noreply.github.com>
2018-04-09 15:16:57 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,13 @@
+# algoritma-stemming-nazief-adriani
+
+
+<?php
+$host='localhost';
+$user='root';
+$pass='';
+$database='dbstbi';
+
+$conn=mysql_connect($host,$user,$pass);
+mysql_select_db($database);
+
+?>
